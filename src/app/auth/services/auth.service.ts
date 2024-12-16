c1c8b68af7c4e2ae193a6aa274825aeb163c0066
Roberto Torres <ingenieria5@lopez-fernandez.com>
2024-12-16 12:01:02 -0700

    .

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { AuthStatus, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject(HttpClient);
+
+  private _currentUser = signal<User|null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+
+
   constructor() { }
+
+  login(email: string, password: string): Observable<boolean>{
+
+    return of(true);
+  }
+
 }
